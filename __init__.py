7ae3fc4cefaa6afed7c79cd1aeb243e2feb14271
narimaneln <92114499+narimaneln@users.noreply.github.com>
2024-02-12 19:26:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/Commits/")
+def mongraphique():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
