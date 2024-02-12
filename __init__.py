7fdcd306ab1ddc63cc573656699f8ddbe466cec6
narimaneln <92114499+narimaneln@users.noreply.github.com>
2024-02-12 18:54:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def MaPremiereAPI():
 def hello_world():
     return render_template('hello.html') #comm3
 
-@app.route('/Temperatures/')
+@app.route('/paris/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
